3a845853e4f5b8e15e0c3481f1616cf6a3614d44
ANKIT YADAV <ANKITYADAV1234567890I@GMAIL.COM>
2024-02-28 08:05:44 +0530

    Time: 35 ms (11.69%) | Memory: 16.1 MB (97.40%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return None
+        
+        queue = [root]
+        leftmost_value = None
+        
+        while queue:
+            level_size = len(queue)
+            leftmost_value = queue[0].val
+            
+            for _ in range(level_size):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return leftmost_value
