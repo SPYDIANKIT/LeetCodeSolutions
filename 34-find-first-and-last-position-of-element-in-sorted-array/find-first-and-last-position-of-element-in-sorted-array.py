f0abacb08178831e1ff92c5d18ac52996a3eaf7b
ANKIT YADAV <ANKITYADAV1234567890I@GMAIL.COM>
2024-03-06 20:46:26 +0530

    Time: 70 ms (6.20%) | Memory: 13.3 MB (26.57%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(len(nums)):
+            if nums[i] == target:
+                result.append(i)
+           
+        if not result:
+            return [-1, -1]
+        else:
+            return [result[0], result[-1]]
