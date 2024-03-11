81984d8d9f8419e15a9c27116f21fa6d2aa631b6
ANKIT YADAV <ANKITYADAV1234567890I@GMAIL.COM>
2024-03-11 11:07:31 +0530

    Time: 334 ms (43.02%) | Memory: 17.4 MB (35.47%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -14,3 +14,10 @@ class Solution(object):
 
         
         return score
+
+
+
+#We iterate over each row of the matrix using a selection sort algorithm.
+# For each row, we find the index of the maximum value in the kth column (k) among the remaining unsorted rows.
+# We swap the current row with the row containing the maximum value found in the kth column.
+# After completing the sorting process, we return the sorted matrix.
